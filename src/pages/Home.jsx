954df8cda45747fe19d79f84fca877f7865cde5a
Sophia Zhang <sophia.p.zhang@gmail.com>
2024-02-10 14:03:41 -0800

    Change paths again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
-import CatCanvas from '../CatCanvas'
+import CatCanvas from "../CatCanvas";
 
 export default function Home() {
-    return (
-        <>
-            <div className='homeTitle'>
-                <div>
-                    <h4>Hello, I'm</h4>
-                    <h1>SOPHIA ZHANG</h1>
-                </div>
-                <CatCanvas></CatCanvas>
-            </div>
-        </>
-
-
-
-    )
+  return (
+    <>
+      <div className="homeTitle">
+        <div>
+          <h4>Hello, I'm</h4>
+          <h1>SOPHIA ZHANG</h1>
+        </div>
+        <CatCanvas></CatCanvas>
+      </div>
+    </>
+  );
 }
